6fe4f8c45eab4c27455f70e82547e5b9ed1d57c7
dominik <dominiksk47@gmail.com>
2025-11-23 18:42:17 +0100

    gitignore

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPhotoSpinner_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appPhotoSpinner")
-set(working_dir "C:/Users/Dominik/Documents/Projects/PhotoSpinner")
-set(src_and_dest_list
-    "C:/Users/Dominik/Documents/Projects/PhotoSpinner/resources.qrc"
-    "C:/Users/Dominik/Documents/Projects/PhotoSpinner/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/PhotoSpinner/resources.qrc"
-
-)
-set(timestamp_file "C:/Users/Dominik/Documents/Projects/PhotoSpinner/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPhotoSpinner_res.txt")
